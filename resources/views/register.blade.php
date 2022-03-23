f3e802b321adc5ba09a3268006684bb647f8220f
anwrtaufik <anwrtaufik@gmail.com>
2022-03-23 21:48:26 +0700

    Minggu 6: register created successfully[tasksdone]

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,20 +5,37 @@
 <div class="main">
     <div class="shop_top">
        <div class="container">
-            <form> 
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+
                     <div class="register-top-grid">
                             <h3>PERSONAL INFORMATION</h3>
                             <div>
-                                <span>First Name<label>*</label></span>
-                                <input type="text"> 
+                                <span>{{ __('Username') }}<label>*</label></span>
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus> 
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div>
-                                <span>Last Name<label>*</label></span>
-                                <input type="text"> 
+                                <span>{{ __('Name') }}<label>*</label></span>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div>
-                                <span>Email Address<label>*</label></span>
-                                <input type="text"> 
+                                <span>{{ __('Email Address') }}<label>*</label></span>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"> 
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="clear"> </div>
                                 <a class="news-letter" href="#">
@@ -30,17 +47,23 @@
                     <div class="register-bottom-grid">
                             <h3>LOGIN INFORMATION</h3>
                             <div>
-                                <span>Password<label>*</label></span>
-                                <input type="text">
+                                <span>{{ __('Password') }}<label>*</label></span>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
                             <div>
-                                <span>Confirm Password<label>*</label></span>
-                                <input type="text">
+                                <span>{{ __('Confirm Password') }}<label>*</label></span>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+
                             </div>
                             <div class="clear"> </div>
                     </div>
                     <div class="clear"> </div>
-                    <input type="submit" value="submit">
+                    <input type="submit" value="{{ __('Register') }}">
             </form>
         </div>
     </div>
